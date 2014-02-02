721c9b653a5d674a926185c0d4d2a0aa26599977
syu93 <herve.tutuaku@gmail.com>
2014-02-02 03:32:43 +0100

    Split bases
    Created background, signIn LogIn, library, language

--- index.php
+++ index.php
@@ -6,8 +6,32 @@
 	<head>
 		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/menu.css">
+		<title>Split</title>
 	</head>
 	<body>
+	
+		<div id="panAcount">
+		
+			<nav class="menu">
+				<ul class="level1">
+					<li><a herf="#"> My Library</a>
+						<ul class="level2">
+							<li><a herf="#"> sql</a>
+								<ul class="level3"><li>Play !</li></ul>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+			
+			<select name="lanquage" class="lanquage">
+				<option> sql </option>
+			</select>
+			
+			<a class="signIn" href="#">Sign In / Log In</a>		
+		</div>
+		
 		<div id="body">
 			<h1>Welcome to Split</h1>
 		</div>
