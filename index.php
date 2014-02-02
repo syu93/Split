484842232883fe291a8bfacd4821e73897361225
syu93 <herve.tutuaku@gmail.com>
2014-02-02 01:29:06 +0100

    Stating Developpement

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div id="body">
+			<h1>Welcome to Split</h1>
+		</div>
+	</body>
+</html>
