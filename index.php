8e8d0d226d5b25f5740491d3cc2cde9eaa467de2
syu93 <herve.tutuaku@gmail.com>
2014-02-04 01:16:22 +0100

    Creat CSS
    Creat CSS: adding Menu and futur slideshow
    
    Pbl: put on the same line(container) brand en menu

--- index.php
+++ index.php
@@ -7,45 +7,52 @@
 		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="stylesheet" type="text/css" href="css/menu.css">
+		<link rel="stylesheet" type="text/css" href="css/slideshow.css">
 		<title>Split</title>
 	</head>
 	<body>
-	
-		<div id="panAcount">
-		
-			<nav class="menu">
-				<ul class="level1">
-					<li><a herf="#"> My Library</a>
-						<ul class="level2">
-							<li><a herf="#"> sqlsqlsqlsqlsqlsqlsql</a>
-								<ul class="level3"><li><a href="#">Play !</a></li></ul>
-							</li>							<li><a herf="#"> sql</a>
-								<ul class="level3"><li><a href="#">Play !</a></li></ul>
-							</li>							<li><a herf="#"> sql</a>
-								<ul class="level3"><li><a href="#">Play !</a></li></ul>
-							</li>							<li><a herf="#"> sql</a>
-								<ul class="level3"><li><a href="#">Play !</a></li></ul>
-							</li>							<li><a herf="#"> sql</a>
-								<ul class="level3"><li><a href="#">Play !</a></li></ul>
-							</li>							<li><a herf="#"> sql</a>
-								<ul class="level3"><li><a href="#">Play !</a></li></ul>
-							</li>							<li><a herf="#"> sql</a>
-								<ul class="level3"><li><a href="#">Play !</a></li></ul>
-							</li>
-						</ul>
-					</li>
-				</ul>
-			</nav>
+			<div id="panAcount">
 			
-			<select name="lanquage" class="lanquage">
-				<option> sql </option>
-			</select>
+				<nav class="menu1">
+					<ul class="level1">
+						<li><a herf="#"> My Library</a>
+							<ul class="level2">
+								<li><a herf="#"> sql</a>
+									<ul class="level3">
+										<li><a href="#">Play !</a>
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+				
+				<select name="lanquage" class="lanquage">
+					<option> sql </option>
+				</select>
+				
+				<a class="signIn" href="#">Sign In / Log In</a>		
+			</div>
 			
-			<a class="signIn" href="#">Sign In / Log In</a>		
-		</div>
-		
+			<div class="slideShow">
+				<h1>Welcome to Split</h1>
+			</div>
+
 		<div id="body">
-			<h1>Welcome to Split</h1>
+			<div class="navBar">
+				<nav class="menu2">
+				<a id="logo" href="index.php"><img  src="img/SPLIT_LOGO.PNG"> </img></a>
+					<ul class="level1">
+						<li>Menu1</li>
+						<li>Menu1</li>
+						<li>Menu1</li>
+						<li>Menu1</li>
+						<li>Menu1</li>
+					</ul>
+				</nav>
+			</div>
+		
 		</div>
 	</body>
 </html>
