983b0719337cd0e825e6ced952d42fe5a6aa3d7e
syu93 <herve.tutuaku@gmail.com>
2014-02-14 19:05:51 +0100

    verify

--- js/checkExist.js
+++ js/checkExist.js
@@ -23,8 +23,34 @@ $( document ).ready(function(){	// Creat Json object
 				{
 					$("#ckPseudo").html("This pseudo is avaible");
 					// alert("Good");
+				}				
+			}, 
+			dataType: "text"
+		});		
+	});
+	
+		$("#mail").change(function(){
+
+	form.mail = $('#mail').val();
+	 // alert(form.pseudo);
+		$.ajax({
+			type: "POST",
+			url: "form/validateSignIn.php",
+			data: form,
+			success: function(data){
+				// alert($('#pseudo').val());
+				// alert(typeof(data));
+				// alert(data);
+				if(data == 'true')
+				{
+					$("#ckMail").html("This mail already exist");
+					// alert("This Pseudo already exist");
 				}
-				
+				else
+				{
+					$("#ckMail").html("This mail is avaible");
+					// alert("Good");
+				}				
 			}, 
 			dataType: "text"
 		});		
