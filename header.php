19d9e69c882ff987577937ae560380723af9390b
syu93 <herve.tutuaku@gmail.com>
2014-02-28 11:56:40 +0100

    CSS

--- header.php
+++ header.php
@@ -1,6 +1,11 @@
 ﻿ <?php
 	require_once("bdd/bddconnect.php");
 	require_once("form/session.php");
+	
+	$lib='SELECT text_fr, text_en FROM content WHERE title="mylibrary" AND active="yes" AND tag ="menu" AND connected="yes"';
+	$gme='SELECT * FROM game WHERE active="yes" AND tag ="game" AND connected="no"';
+	$ply='SELECT text_fr, text_en FROM content WHERE title="play" AND active="yes" AND tag ="menu" AND connected="yes"';
+	$sign='SELECT text_fr, text_en FROM content WHERE title="signup" AND active="yes" AND tag ="text" AND connected="no"';
  ?>
  <!DOCTYPE html>
 <html>
@@ -27,22 +32,14 @@
 			<div id="panAcount">			
 				<nav class="menu1">
 					<ul class="level1">
-	<?php
-	$library = $bdd->query('SELECT text_fr, text_en FROM content WHERE title="mylibrary" AND active="yes" AND tag ="menu" AND connected="yes"');
-	while ($donnees = $library->fetch())
-	{
-	?>
-						<li><a herf="#"><?php echo$donnees[$_SESSION['user']['langue']];?></a>
-	<?php
-	}
-	?>
+	<?php	$library = $bdd->query($lib);
+	while ($donnees = $library->fetch()) { ?>
+						<li><a class="signIn" herf="#"><?php echo$donnees[$_SESSION['user']['langue']];?></a>
+	<?php }	?>
 							<ul class="level2">
-								<li class="myGame">
-	<?php
-	$game = $bdd->query('SELECT * FROM game WHERE active="yes" AND tag ="game" AND connected="no"');
-	while ($donnees = $game->fetch())
-	{
-	?>
+								
+	<?php	$game = $bdd->query($gme);
+	while ($donnees = $game->fetch()) {	?>
 								<li class="myGame">
 									<span class="g-img">
 										<img src="<?php echo$donnees["url"];?>"/>
@@ -50,37 +47,26 @@
 									<span class="g-desc">
 										<a href="#"><?php echo$donnees[$_SESSION['user']['langue']];?></a>									
 									</span>
-	<?php }	/*
-	$play = $bdd->query('SELECT text_fr, text_en FROM content WHERE title="play" AND active="yes" AND tag ="menu" AND connected="yes"');
-	while ($donnees = $play->fetch())
-	{*/
-	?>
+		<?php	$play = $bdd->query($ply);
+		while ($donnees = $play->fetch()) {	?>
 									<ul class="level3">
 										<li>
 											<a href="#"><?php echo$donnees[$_SESSION['user']['langue']];?></a>
-	<?php
-	// } 
-	?>
-	<?php
-	// } 
-	?>
+		<?php }	?>
+
 
 										</li>
 									</ul>
 								</li>
+	<?php } ?>
 							</ul>
 						</li>
 					</ul>
 				</nav>
-	<?php
-	$signup = $bdd->query('SELECT text_fr, text_en FROM content WHERE title="signup" AND active="yes" AND tag ="text" AND connected="no"');
-	while ($donnees = $signup->fetch())
-	{
-	?>				
+	<?php	$signup = $bdd->query($sign);
+	while ($donnees = $signup->fetch()) { ?>				
 				<span id="signIn" class="signIn"><?php echo$donnees[$_SESSION['user']['langue']];?></span>
-	<?php
-	}
-	?>			
+	<?php } ?>			
 				<form method="POST" action="form/validateSignUp.php" id="formLang">
 					<select id="langue" name="langue" class="language">
 					<?php
@@ -99,37 +85,38 @@
 				</form>
 			</div>
 				<?php 
-					//require_once("connect.php");
+					require_once("connect.php");
 				?>
 			</div>
-			<!--
+			
 			<div class="carousel-header">
 				<div class="slideShow">
 					<h1>Welcome to Split</h1>
 				</div>
 			</div>
-			<nav class="menu2">						
-				<ul class="level1">
-					<span><a id="logo" href="index.php"><img  src="img/SPLIT_LOGO.PNG"></a></span>
-					
-					<?php
-					if($_SESSION['user']['connected']==false)
-					{
-					$reponse = $bdd->query('SELECT * FROM content WHERE active="yes" AND tag ="menu" AND connected="no" ORDER BY position');
-					}
-					else if($_SESSION['user']['connected']==true)
-					{
-					$reponse = $bdd->query('SELECT * FROM content WHERE active="yes" AND tag ="menu" AND connected="yes" ORDER BY position');
-					}
-					// var_dump ($reponse);
-					while ($donnees = $reponse->fetch())
+			<div class="element-container">
+				<nav class="menu2 element-container">					
+					<ul class="level1">
+						<span><a id="logo" href="index.php"><img  src="img/SPLIT_LOGO.PNG"></a></span>
+						
+						<?php
+						if($_SESSION['user']['connected']==false)
 						{
-					?>						
-						<li><a href="action.php">	<?php echo $donnees[$_SESSION['user']['langue']]; ?>	</a></li>
-					<?php
+						$reponse = $bdd->query('SELECT * FROM content WHERE active="yes" AND tag ="menu" AND connected="no" ORDER BY position');
 						}
-					?>
-					
-					<li class="cart"><a href="game.php">My Cart</a></li>
-				</ul>
-			</nav>
+						else if($_SESSION['user']['connected']==true)
+						{
+						$reponse = $bdd->query('SELECT * FROM content WHERE active="yes" AND tag ="menu" AND connected="yes" ORDER BY position');
+						}
+						// var_dump ($reponse);
+						while ($donnees = $reponse->fetch())
+							{
+						?>						
+							<li><a href="action.php">	<?php echo $donnees[$_SESSION['user']['langue']]; ?>	</a></li>
+						<?php
+							}
+						?>
+						
+						<li class="cart"><a href="game.php">My Cart</a></li>
+					</ul>
+				</nav>
